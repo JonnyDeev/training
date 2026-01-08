15d71bd19efa6a7925a7aa427bf3e329b77bddb9
JonnyDeev <jonnyp.dev@gmail.com>
2026-01-07 21:41:33 -0500

    more js training

--- Learning/Js/array_methods.js
+++ Learning/Js/array_methods.js
@@ -1,9 +1,38 @@
 // MAP
 
-
-let a1 = [2,4,5,6]
+let a1 = [2, 4, 5, 6];
 
 a1.map((e) => {
-  console.log(e+1); 
-})
+  console.log(e + 1);
+});
 
+// POP
+let a2 = [3, 5, 6, 7, 8];
+console.log(a2); //let a2 = [3,5,6,7,8]
+a2.pop();
+
+console.log(a2); //let a2 = [3,5,6,7]
+
+//SLICE
+
+const arr3 = [0, 10, 1, 2, 3, 4];
+
+console.log(arr3); //[0,10,1,2,3,4]
+
+const arr4 = arr3.slice(1, 3);
+
+console.log(arr4); // [ 10, 1 ]
+
+//SPLICE
+
+const arr5 = [1, 2, 3, 4, 5];
+console.log(arr5)
+arr5.splice(2,1)
+console.log(arr5);
+
+
+// SHIFT
+
+const shift_array = [1,2,3,4,5]
+shift_array.shift()
+console.log(shift_array) //[ 2, 3, 4, 5 ]

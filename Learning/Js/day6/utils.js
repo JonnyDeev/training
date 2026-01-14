1c5267cdd0f64e6708e19511e25fc5464780054b
JonnyDeev <jonnyp.dev@gmail.com>
2026-01-13 21:50:23 -0500

    Add user data and utility functions for user analysis

---
+++ Learning/Js/day6/utils.js
@@ -0,0 +1,33 @@
+import { users } from "./data.js";
+
+// Create and export:
+
+// getActiveUsers(users)
+
+// getAverageAge(users)
+
+// formatUserNames(users)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const getActiveUsers = (users) => {
+//   let activeUsers = users.filter((user) => {
+//     return user.active === true;
+//   });
+//   console.log("current active users: ", activeUsers.length);
+//   return activeUsers;
+// };
+
+// console.log(getActiveUsers(users));
+
+const getAverageAge = (users) => {
+  const userAges = users.map((user) => user.age);
+  const sumAges = userAges.reduce((acc, userAge) => {
+    return acc + userAge;
+  });
+  const averageAge = sumAges / users.length;
+  console.log(userAges);
+  console.log(averageAge.toFixed(0));
+};
+
+getAverageAge(users);

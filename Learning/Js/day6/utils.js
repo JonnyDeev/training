367f144dac4f6434bd7b21a6f9c276c767f084be
JonnyDeev <jonnyp.dev@gmail.com>
2026-01-14 21:38:51 -0500

    Update comments for user utility functions and format user names output

--- Learning/Js/day6/utils.js
+++ Learning/Js/day6/utils.js
@@ -2,9 +2,9 @@ import { users } from "./data.js";
 
 // Create and export:
 
-// getActiveUsers(users)
+// getActiveUsers(users) ✅
 
-// getAverageAge(users)
+// getAverageAge(users)✅
 
 // formatUserNames(users)
 
@@ -20,14 +20,30 @@ import { users } from "./data.js";
 
 // console.log(getActiveUsers(users));
 
-const getAverageAge = (users) => {
-  const userAges = users.map((user) => user.age);
-  const sumAges = userAges.reduce((acc, userAge) => {
-    return acc + userAge;
-  });
-  const averageAge = sumAges / users.length;
-  console.log(userAges);
-  console.log(averageAge.toFixed(0));
-};
+// const getAverageAge = (users) => {
+//   const userAges = users.map((user) => user.age);
+//   const sumAges = userAges.reduce((acc, userAge) => {
+//     return acc + userAge;
+//   });
+//   const averageAge = sumAges / users.length;
+//   console.log(userAges);
+//   console.log(averageAge.toFixed(0));
+// };
 
-getAverageAge(users);
+// getAverageAge(users);
+
+// FORMAT USER NAMES
+
+// const fullNames = users.map((user) => {
+//   return { fullName: `${user.first_name} ${user.last_name}` };
+// });
+
+const formattedUserNames = users.map(
+  ({ first_name, last_name, id, active, age }) => ({
+    id,
+    full_name: `${first_name} ${last_name}`,
+    age,
+    active,
+  })
+);
+console.log(formattedUserNames);

6322c24088575c91bef18ac719a996f96705e6fe
JonnyDeev <111886518+JonnyDeev@users.noreply.github.com>
2026-01-19 19:46:38 -0500

    Add initial package.json and tasks.js files; implement user retrieval in express server

--- Learning/week 2/express_server.js
+++ Learning/week 2/express_server.js
@@ -2,6 +2,8 @@
 
 import express from "express";
 
+import { users } from "../Js/day6/data.js";
+
 //create express app
 const app = express();
 
@@ -12,10 +14,21 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.json({ message: "API running" });
 });
-app.get("/health", (req, res) => {
-  res.json({ status: 200, message: "Url Working" });
+app.get("/products/:id", (req, res) => {
+  const idProducto = req.params.id;
+  res.send(`You are trying to access product ${idProducto}`);
 });
 
+app.get("/users/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+
+  const userData = users.find((user) => user.id === userId);
+  if (!userData) {
+    res.send("user not found");
+  } else {
+    res.json(userData);
+  }
+});
 //start server
 
 app.listen(port, () => {

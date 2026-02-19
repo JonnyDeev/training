dc3984236a0791163a47528a4c6489ced0f332d4
JonnyDeev <jonnyp.dev@gmail.com>
2026-02-18 21:07:36 -0500

    feat: enhance App component with advanced counter features and UI improvements

--- Learning/React/react-advanced-counter/src/App.jsx
+++ Learning/React/react-advanced-counter/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [simpleCounter, setSimpleCounter] = useState(0);
+  // const [stepsValue, setStepsValue] = useState(1);
 
   const handleIncrease = () => {
     setSimpleCounter(simpleCounter + 1);
@@ -15,6 +16,10 @@ function App() {
   const handleReset = () => {
     setSimpleCounter(0);
   };
+
+  // // const handleSteps = () => {
+  //   stepsValue == setStepsValue()
+  // }
   return (
     <>
       {/* Container */}
@@ -60,11 +65,52 @@ function App() {
 
           {/* Advanced Counter */}
           <div className="bg-cyan-500 rounded-xl flex flex-col items-center justify-center w-1/2">
-            <p>Simple Counter</p>
-            <h4>0</h4>
-            <button>Decrease</button>
-            <button>Reset</button>
-            <button>Increase</button>
+            <div className="bg-gray-100 border border-gray-300 shadow-lg rounded-xl flex flex-col items-center justify-between w-1/2 h-80 py-8">
+              <p className="text-xl text-gray-600">Advanced Counter</p>
+              <h4 className="text-5xl font-bold text-blue-400">
+                {simpleCounter}
+              </h4>
+              <div>
+                <input
+                  type="range"
+                  name="steps"
+                  id="steps"
+                  max={20}
+                  // value={stepsValue}
+                />
+                <p></p>
+              </div>
+              <div className="flex">
+                <button
+                  className="bg-red-400 border border-gray-400 rounded-lg p-2 w-1/3 px-4 text-center mx-2 font-bold text-gray-900 "
+                  onClick={handleDecrease}
+                >
+                  -1
+                </button>
+                <button
+                  className="bg-gray-200 border border-gray-400 rounded-lg p-2 w-1/3 px-4 text-center mx-2 font-bold text-gray-500 "
+                  onClick={handleReset}
+                >
+                  Undo
+                </button>
+                <button
+                  className="bg-gray-200 border border-gray-400 rounded-lg p-2 w-1/3 px-4 text-center mx-2 font-bold text-gray-500 "
+                  onClick={handleReset}
+                >
+                  Reset
+                </button>
+                <button
+                  className="bg-blue-400 border border-gray-400 rounded-lg p-2 w-1/3 px-4 text-center mx-2 font-bold text-gray-900 "
+                  onClick={handleIncrease}
+                >
+                  +1
+                </button>
+              </div>
+              <div>
+                <p>History</p>
+                <div>[1],[2]</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
